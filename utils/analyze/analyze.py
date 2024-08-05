6f578f40099086cf086223eb21bd81127f933e38
hotnops <hotnops@protonmail.com>
2024-08-04 20:12:42 -0700

    Removing unnecessary analyze code and including the assumerole analyze endpoint request

--- utils/analyze/analyze.py
+++ utils/analyze/analyze.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import arn
 import re
+import requests
 
 from neo4j import GraphDatabase
 
@@ -122,264 +123,9 @@ def populate_resource_types(session):
     """
     session.run(cypher_query)
 
-def get_allow_assume_role_statements(session):
-    print("[*] Getting assume role statements")
-    # First, get a pairing of all the allow roles to resources
-    print("[*] Creating ALLOW_PERMISSIONS_STS_ASSUMEROLE relationships")
-    query = """
-        MATCH (prin) <- [:AttachedTo*2..3] - (:AWSPolicyDocument) 
-            <- [:AttachedTo] - (s:AWSStatement) 
-            - [:Action|ExpandsTo*1..2] -> (a:AWSAction {name: 'sts:assumerole'})
-        WHERE (prin:AWSRole OR prin:AWSUser OR prin:AWSGroup)
-
-        WITH prin, s,
-            CASE WHEN s.effect = "Allow" THEN s ELSE NULL END as allowStatement,
-            CASE WHEN s.effect = "Deny" AND NOT EXISTS ((s) <- [:AttachedTo] - (:AWSCondition)) THEN s ELSE NULL END as denyStatement
-
-        WITH prin,
-            COLLECT(allowStatement) as allowStatements,
-            COLLECT(denyStatement) as denyStatements
-
-        UNWIND allowStatements as allowSt
-        MATCH (allowSt)-[:Resource|ExpandsTo*1..2]->(r:AWSRole)
-        WITH DISTINCT r, prin, denyStatements, allowSt
-        WHERE NONE(denySt IN denyStatements WHERE (denySt)-[:Resource]->(r) OR (denySt)-[:Resource]->(:AWSResourceBlob)-[:ExpandsTo]->(r))
-        MERGE (prin) - [:AllowPermissionSTSAssumeRole {layer: 2, statement:allowSt.hash}] -> (r)
-    """
-    session.run(query)
-
-    print("[*] Creating ALLOW_TRUST_STS_ASSUMEROLE relationships")
-    query = """
-        MATCH (prin:AWSRole) <- [:AttachedTo] - (:AWSAssumeRolePolicy) 
-            <- [:AttachedTo] - (s:AWSStatement) 
-            - [:Action|ExpandsTo*1..2] -> (a:AWSAction {name: 'sts:assumerole'})
-
-        WITH prin, s,
-            CASE WHEN s.effect = "Allow" THEN s ELSE NULL END as allowStatement,
-            CASE WHEN s.effect = "Deny" AND NOT EXISTS ((s) <- [:AttachedTo] - (:AWSCondition)) THEN s ELSE NULL END as denyStatement
-
-        WITH prin,
-            COLLECT(allowStatement) as allowStatements,
-            COLLECT(denyStatement) as denyStatements
-
-        UNWIND allowStatements as allowSt
-
-        MATCH (allowSt)-[:Resource|ExpandsTo*1..2]->(r:UniqueArn)
-        WITH DISTINCT r, prin, denyStatements, allowSt
-        WHERE NONE(denySt IN denyStatements WHERE (denySt)-[:Resource]->(r) OR (denySt)-[:Resource]->(:AWSResourceBlob)-[:ExpandsTo]->(r))
-        MERGE (prin) - [:AllowTrustSTSAssumeRole {layer: 2, statement:allowSt.hash}] -> (r)
-    """
-    session.run(query)
-    
-    print("[*] Creating CAN_ASSUME relationships")
-    query = """
-    MATCH (a) - [:AllowPermissionSTSAssumeRole] -> (b) WHERE (b) - [:AllowTrustSTSAssumeRole] -> (a)
-    MERGE (a) - [:CanAssume {layer: 2} ] -> (b)
-    """
-    session.run(query)
-
-    query = """
-    MERGE (source) - [:CanAssume {layer: 2} ] -> (dest)
-    """
-    session.run(query)
-
-    return None
-
-
-def create_can_assume_edge(statement):
-
-    resources = get_associated_resources(statement)
-
-
-def rsop_check(roles_to_statements, statements_to_actions):
-    rsop_map = {}
-    for principal_arn, statement_hashes in roles_to_statements.items():
-
-        prinipal_arn, statement_hash, statement_action = entry
-        principal_entry = rsop_map.get(prinipal_arn, {'Allow': [], 'Deny': []})
-        principal_entry[statement_action].append(statements_to_actions[statement_hash])
-    
-    return rsop_map
-
-def query_results_to_dictionary(results):
-    ret_dict = {}
-    for entry in results:
-        hash_list = ret_dict.get(entry[0], [])
-        hash_list.append(entry[1])
-        ret_dict[entry[0]] = hash_list
-
-    return ret_dict
-
-
-def get_tier_zero_roles(session):
-    # This query gets all principals that have statements that permit
-    # Attach and Detach a user role policies to themselves
-    query = (
-        'MATCH (r:AWSRole) <- [:Resource|ExpandsTo*1..2] - (s:AWSStatement) - [:AttachedTo] -> (:AWSPolicyDocument) - [:AttachedTo*1..3] -> (r) '
-        'WITH r,COLLECT(s) as statements '
-        'MATCH p=(r) <- [:AttachedTo*1..3] - (:AWSPolicyDocument) <- [:AttachedTo] - (s) - [:Action|ExpandsTo*1..2] -> (:AWSAction {name: "iam:attachrolepolicy"}) '
-        'WHERE s in statements '
-        'WITH COLLECT(s) as attachrolestatements, r, statements '
-        'MATCH p2=(r) <- [:AttachedTo*1..3] - (:AWSPolicyDocument) <- [:AttachedTo] - (s) - [:Action|ExpandsTo*1..2] -> (:AWSAction {name: "iam:detachrolepolicy"}) '
-        'WHERE s in statements '
-        'WITH COLLECT(s) as detachrolestatements, r, attachrolestatements, statements '
-        'RETURN DISTINCT r.arn, attachrolestatements, detachrolestatements '
-    )
-
-    query_results = session.run(query).values()
-    allow_arns = [role_arn[0] for role_arn in query_results]
-
-    query = (
-        'MATCH (r:AWSRole) <- [:Resource|ExpandsTo*1..2] - (s:AWSStatement) - [:AttachedTo] -> (:AWSPolicyDocument) - [:AttachedTo*1..3] -> (r) '
-        'WITH r,COLLECT(s) as statements '
-        'MATCH p=(r) <- [:AttachedTo*1..3] - (:AWSPolicyDocument) <- [:AttachedTo] - (s) - [:DenyAction|ExpandsTo*1..2] -> (a:AWSAction) '
-        'WHERE s in statements AND a.name in ["iam:attachrolepolicy", "iam:detachrolepolicy"] '
-        'RETURN DISTINCT r.arn, s '
-    )
-
-    query_results = session.run(query).values()
-    deny_arns = [role_arn[0] for role_arn in query_results]
-
-    t0_roles = [t0_arn for t0_arn in allow_arns if t0_arn not in deny_arns]
-    for t0_role in t0_roles:
-        query = f'MATCH (a:AWSRole) WHERE a.arn = "{t0_role}" SET a.tier_zero = true'
-        query_results = session.run(query)
-        print(query_results)
-    return t0_roles
-
-    # We now need to check if there is a deny
-
-def get_tier_zero_users(session):
-    # This query gets all principals that have statements that permit
-    # Attach and Detach a user role policies to themselves
-    query = (
-        'MATCH (r:AWSUser) <- [:Resource|ExpandsTo*1..2] - (s:AWSStatement) - [:AttachedTo] -> (:AWSPolicyDocument) - [:AttachedTo*1..3] -> (r) '
-        'WITH r,COLLECT(s) as statements '
-        'MATCH p=(r) <- [:AttachedTo*1..3] - (:AWSPolicyDocument) <- [:AttachedTo] - (s) - [:Action|ExpandsTo*1..2] -> (:AWSAction {name: "iam:attachuserpolicy"}) '
-        'WHERE s in statements '
-        'WITH COLLECT(s) as attachstatements, r, statements '
-        'MATCH p2=(r) <- [:AttachedTo*1..3] - (:AWSPolicyDocument) <- [:AttachedTo] - (s) - [:Action|ExpandsTo*1..2] -> (:AWSAction {name: "iam:detachuserpolicy"}) '
-        'WHERE s in statements '
-        'WITH COLLECT(s) as detachstatements, r, attachstatements, statements '
-        'RETURN DISTINCT r.arn, attachstatements, detachstatements '
-    )
-
-    query_results = session.run(query).values()
-    allow_arns = [user_arn[0] for user_arn in query_results]
-
-    query = (
-        'MATCH (r:AWSUser) <- [:Resource|ExpandsTo*1..2] - (s:AWSStatement) - [:AttachedTo] -> (:AWSPolicyDocument) - [:AttachedTo*1..3] -> (r) '
-        'WITH r,COLLECT(s) as statements '
-        'MATCH p=(r) <- [:AttachedTo*1..3] - (:AWSPolicyDocument) <- [:AttachedTo] - (s) - [:DenyAction|ExpandsTo*1..2] -> (a:AWSAction) '
-        'WHERE s in statements AND a.name in ["iam:attachuserpolicy", "iam:detachuserpolicy"] '
-        'RETURN DISTINCT r.arn, s '
-    )
-
-    query_results = session.run(query).values()
-    deny_arns = [user_arn[0] for user_arn in query_results]
-
-    return [t0_arn for t0_arn in allow_arns if t0_arn not in deny_arns]
-
-def get_tier_zero_groups(session):
-    # This query gets all principals that have statements that permit
-    # Attach and Detach a user role policies to themselves
-    query = (
-        'MATCH (r:AWSGroup) <- [:Resource|ExpandsTo*1..2] - (s:AWSStatement) - [:AttachedTo] -> (:AWSPolicyDocument) - [:AttachedTo*1..3] -> (r) '
-        'WITH r,COLLECT(s) as statements '
-        'MATCH p=(r) <- [:AttachedTo*1..3] - (:AWSPolicyDocument) <- [:AttachedTo] - (s) - [:Action|ExpandsTo*1..2] -> (:AWSAction {name: "iam:attachgrouppolicy"}) '
-        'WHERE s in statements '
-        'WITH COLLECT(s) as attachstatements, r, statements '
-        'MATCH p2=(r) <- [:AttachedTo*1..3] - (:AWSPolicyDocument) <- [:AttachedTo] - (s) - [:Action|ExpandsTo*1..2] -> (:AWSAction {name: "iam:detachgrouppolicy"}) '
-        'WHERE s in statements '
-        'WITH COLLECT(s) as detachstatements, r, attachstatements, statements '
-        'RETURN DISTINCT r.arn, attachstatements, detachstatements '
-    )
-
-    query_results = session.run(query).values()
-    allow_arns = [group_arn[0] for group_arn in query_results]
-
-    query = (
-        'MATCH (r:AWSGroup) <- [:Resource|ExpandsTo*1..2] - (s:AWSStatement) - [:AttachedTo] -> (:AWSPolicyDocument) - [:AttachedTo*1..3] -> (r) '
-        'WITH r,COLLECT(s) as statements '
-        'MATCH p=(r) <- [:AttachedTo*1..3] - (:AWSPolicyDocument) <- [:AttachedTo] - (s) - [:DenyAction|ExpandsTo*1..2] -> (a:AWSAction) '
-        'WHERE s in statements AND a.name in ["iam:attachgrouppolicy", "iam:detachgrouppolicy"] '
-        'RETURN DISTINCT r.arn, s '
-    )
-
-    query_results = session.run(query).values()
-    deny_arns = [user_arn[0] for user_arn in query_results]
-
-    return [t0_arn for t0_arn in allow_arns if t0_arn not in deny_arns]
-
-
-def get_principals_on_resource(session, resourceArn: str):
-    # Get all statements that affect the provided principal
-    query = (
-        f'MATCH (s:AWSStatement), (u:UniqueArn {{arn: "{resourceArn}"}}) '
-        'WHERE (s) - [:Resource] -> (u) OR '
-        '(s) - [:Resource] -> (:AWSResourceBlob) - [:ExpandsTo] -> (u) '
-        'WITH s,u '
-        # Check if the statement actually has an action that can act on the resource
-        'MATCH (u) - [:TypeOf] -> (:AWSResourceType) <- [:ActsOn] - (a:AWSAction) <- [:Action|ExpandsTo*1..2] - (s) '
-        'WITH COLLECT(s) as statements, a,u '
-        'MATCH  (s2:AWSStatement) - [:AttachedTo] -> (:AWSPolicyDocument) - [:AttachedTo|MemberOf*1..4] -> (r) '
-        'WHERE s2 IN statements AND (r:AWSRole OR r:AWSUser OR r:AWSGroup) '
-        # Typical policy access only affects resources within the same account
-        # Empty account ID check is for S3 buckets
-        'AND ((r.account_id = u.account_id) OR (u.account_id = "")) '
-        'RETURN DISTINCT r.arn,s2.hash,s2.effect,a.name'
-    )
-
-    # The returnted statements are any statement that has the target ARN in scope
-    # AND has at least one action that affects that resource
-    query_result = session.run(query)
-    principal_permissions_dict = {}
-    for entry in query_result:
-        principal_arn, statement_hash, statement_effect, action_name = entry
-        rsop = principal_permissions_dict.get(principal_arn, {'Allow': {},
-                                                              'Deny': {},
-                                                              'ConditionalAllow': {},
-                                                              'ConditionalDeny': {}})
-        try:
-            hash_list = rsop[statement_effect].get(action_name, [])
-        except:
-            import pdb; pdb.set_trace()
-        hash_list.append(statement_hash)
-        rsop[statement_effect][action_name] = hash_list
-        principal_permissions_dict[principal_arn] = rsop
-
-    return_dict = {}
-    for arn, rsop in principal_permissions_dict.items():
-        rsop_actions = [x for x in rsop['Allow'].keys() if x not in rsop['Deny'].keys()]
-        if rsop_actions:
-            action_to_statements = {}
-            for rsop_action in rsop_actions:
-                action_to_statements[rsop_action] = rsop["Allow"][rsop_action]
-            return_dict[arn] = action_to_statements
-
-    return return_dict
 
 def populate_arn_fields(session):
     print("[*] Extrapolating ARN properties")
-    # results = session.run("MATCH (u:UniqueArn) RETURN u")
-    # for result in results:
-    #     try:
-    #         node_arn_string = result['u']['arn']
-    #         node_arn = arn.Arn.fromstring(node_arn_string)
-    #         query = (
-    #             f'MATCH (u:UniqueArn {{arn: "{node_arn_string}"}}) '
-    #             f'SET u.partition = "{node_arn.partition}" '
-    #             f'SET u.service =  "{node_arn.service}" '
-    #             f'SET u.region = "{node_arn.region}" '
-    #             f'SET u.account_id = "{node_arn.account_id}" '
-    #             f'SET u.resource = "{node_arn.resource}" '
-    #             f'MERGE (a:AWSAccount {{account_id: "{node_arn.account_id}", layer: 2}}) '
-    #             f'MERGE (u) - [:InAccount {{layer: 2}}] -> (a)'
-    #         )
-
-    #         session.run(query)
-    #     except ValueError:
-    #         continue
     cypher_query = """
     CALL apoc.periodic.iterate(
         "MATCH (u:UniqueArn) RETURN u",
@@ -398,6 +144,14 @@ def populate_arn_fields(session):
     """
     session.run(cypher_query)
 
+def analyze_assume_roles():
+    print("[*] Analyzing assume roles")
+    resp = requests.get("http://apeman-backend.localhost/analyze/assumeroles")
+    if resp.status_code == 200:
+        print("[*] Assume role analysis complete")
+    else:
+        print("[!] Could not analyze assume roles")
+        import pdb; pdb.set_trace()
 
 def analyze():
     driver = GraphDatabase.driver("bolt://localhost:7687",
@@ -411,15 +165,11 @@ def analyze():
         populate_resource_blob(session)
         populate_principal_blob(session)
         #populate_not_resources(session)
-        #(session)
 
-        # For process each statement and find unconditional
-        # sts:assumerole actions and map them to the resources
-        # identified in that statement
-
-        #statements = get_allow_assume_role_statements(session)
-        #for statement in statements:
-        #    create_can_assume_edge(statement)
+        # This is the first migration into server
+        # based analysis. All others will move over
+        # to API calls
+        analyze_assume_roles()
 
 
 if __name__ == "__main__":
